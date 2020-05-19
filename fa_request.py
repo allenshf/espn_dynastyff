14d3e7ee0ee0c0139d177c1c22d02d579da16ea6
allenshf <45723095+allenshf@users.noreply.github.com>
2020-05-19 14:56:48 -0500

    Retrieves list of top free agents from 2020 league
    League must be set to public, espn-api must be installed first

---
+++ fa_request.py
@@ -0,0 +1,18 @@
+#First need to import espn_api library to computer
+#Use command: pip insatall espi_api
+from espn_api.football import League
+import pandas as pd
+
+#Request league info from API
+league = League(league_id = 331933, year = 2020)
+
+#Collect list of top free agents
+fa = league.free_agents(size=150)
+
+#Create list of what info we want
+fa_info = [[fa.index(player), player.name, player.proTeam, player.position,
+           player.projected_points, player.points] for player in fa]
+
+#Turn list into table
+fa_df = pd.DataFrame(fa_info, columns = ['Rank', 'Name', 'Team', 'Position', 'Projected Points', 'Points'])
+fa_df = fa_df.set_index('Rank')

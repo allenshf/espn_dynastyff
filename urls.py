f164050f08c05fe26a52bec0d5729e716e0d60df
allenshf <45723095+allenshf@users.noreply.github.com>
2020-05-22 23:03:15 -0500

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='draft-home'),
-    path('draft/', views.draft, name='draft-room'),
-    path('draft/<int:id>/', views.access, name='draft-room')
-]

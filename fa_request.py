aebf2f9c978a94aff8d348da1d03ddaa1eca9c9b
allenshf <45723095+allenshf@users.noreply.github.com>
2020-05-22 23:03:45 -0500

    Update Storage of Team Names in sql

--- fa_request.py
+++ fa_request.py
@@ -4,23 +4,30 @@ from espn_api.football import League
 import pandas as pd
 import sqlite3
 from os import path
+import sys
 
 #Establish SQL connection
-DATA_DIR = '' #Insert local directory path for sqlite database
+DATA_DIR = 'C:/Users/dude0/Desktop/RookieDraft/rookiedraft' #Insert local directory path for sqlite database
 conn = sqlite3.connect(path.join(DATA_DIR, 'freeagent.sqlite'))
 
 #Request league info from API
-league = League(league_id = 331933, year = 2020)
+leagueID = int(sys.argv[1])
+league = League(league_id = leagueID, year = 2020)
 
 #Collect list of top free agents
 fa = league.free_agents(size=150)
 
-#Create list of what info we want
+#Create list of what player info we want
 fa_info = [[fa.index(player) + 1, player.name, player.proTeam, player.position,
-           player.projected_points, player.points] for player in fa]
+           player.projected_points, player.points, False] for player in fa]
+
+
+#Create list of all owners
+teams = [team.team_name for team in league.teams]
 
 #Turn list into table
-fa_df = pd.DataFrame(fa_info, columns = ['Rank', 'Name', 'Team', 'Position', 'Projected Points', 'Points'])
-fa_df = fa_df.set_index('Rank')
+fa_df = pd.DataFrame(fa_info, columns = ['Rank', 'Name', 'Team', 'Position', 'Projected_Points', 'Points', 'Drafted'])
+team_df = pd.DataFrame(teams, columns = ['Name'])
 
 fa_df.to_sql('free_agents', conn, index = False, if_exists = 'replace')
+team_df.to_sql('teams', conn, index=False, if_exists='replace')

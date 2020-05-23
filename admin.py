da8995f3312bdcf6507c1c1678b364a5d596a0f8
allenshf <45723095+allenshf@users.noreply.github.com>
2020-05-22 23:02:34 -0500

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Pick,Player,League
-
-# Register your models here.
-admin.site.register(Pick)
-admin.site.register(League)
-admin.site.register(Player)

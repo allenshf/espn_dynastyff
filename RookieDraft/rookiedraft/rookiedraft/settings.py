e39477dacb01db2fad4658cfd80b73d866c3afcd
allenshf <45723095+allenshf@users.noreply.github.com>
2020-05-24 01:54:13 -0500

    New login-required page with error message

--- RookieDraft/rookiedraft/rookiedraft/settings.py
+++ RookieDraft/rookiedraft/rookiedraft/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'draft.apps.DraftConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'draft-home'
+
+LOGIN_URL = 'login-req'
--- RookieDraft/rookiedraft/rookiedraft/urls.py
+++ RookieDraft/rookiedraft/rookiedraft/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
-from . import views
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 urlpatterns = [
     url('admin/', admin.site.urls),
+    url('register/', user_views.register, name='register'),
+    url('profile/', user_views.profile, name='profile'),
+    url('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url('login-req/', auth_views.LoginView.as_view(template_name='users/login_required.html'), name='login-req'),
+    url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     url('',include('draft.urls')),
 ]

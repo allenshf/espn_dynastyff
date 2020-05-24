dcef2f5d60cc85c19a7f78175d112504e50a2ffd
allenshf <45723095+allenshf@users.noreply.github.com>
2020-05-24 15:13:48 -0500

    Updated user profile page to display user's leagues
    Also added comments to code

--- RookieDraft/rookiedraft/users/views.py
+++ RookieDraft/rookiedraft/users/views.py
@@ -3,8 +3,10 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
-# Create your views here.
+#User Registration Page
 def register(request):
+
+    #If form was sent, validate
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
@@ -12,11 +14,12 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, 'Your account has been created!')
             return redirect('login')
-
+    #Else, display user registration form
     else: 
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+#Profile page
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
